73ff17c6c1ef18ea9b12407544554e323b770580
Glen Cheney <glen@odopod.com>
2017-08-03 10:44:46 -0700

    Remove Firefox launcher.
    Last build was 10 minutes with Chrome (headless) and Firefox. Let's see how long this ones is.

--- karma.conf.js
+++ karma.conf.js
@@ -70,7 +70,7 @@ module.exports = (config) => {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['ChromeHeadless', 'Firefox'],
+    browsers: ['ChromeHeadless'],
 
 
     // Continuous Integration mode
